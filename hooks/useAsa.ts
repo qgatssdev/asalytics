6cb7f8c46c6b39eaeee402667061524c5af3132c
Uche Omodu <chivoomodu@gmail.com>
2022-08-11 23:36:22 +0100

    custom hook for Asa

---
+++ hooks/useAsa.ts
@@ -0,0 +1,15 @@
+import { useQuery } from '@tanstack/react-query';
+import { GraphQLClient } from 'graphql-request';
+
+import { GET_ALL_USERS } from '../queries/get-all';
+
+const endpoint = 'https://analytics-api.herokuapp.com/analytics';
+
+export function useAsa() {
+  const client = new GraphQLClient(endpoint);
+
+  return useQuery(['Asas'], async () => {
+    const { asalist } = await client.request(GET_ALL_USERS);
+    return asalist;
+  });
+}
