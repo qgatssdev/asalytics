a4abb8a804a26d2c90899d3ee17b90da19708df1
Uche Omodu <chivoomodu@gmail.com>
2022-08-15 13:57:50 +0100

    header content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,9 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.scss';
 import Image from 'next/image';
-import { useAsa } from '../hooks/useAsa';
-import { useEffect } from 'react';
+import Asalytics from '../components/asalytics';
 
 export default function Home() {
-  const { data, isLoading, error } = useAsa();
-
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
   return (
     <div className={styles.container}>
       <Head>
@@ -24,12 +18,13 @@ export default function Home() {
           width={150}
           height={62.94}
         />
-        <button>ANALYSE ASAs</button>
+        <button className={styles.header__button}>ANALYSE ASAs</button>
       </header>
       <main>
         <div className={styles.heading}>
           <h1>List of Algorand Standard Assets on ASAlytics</h1>
         </div>
+        <Asalytics />
       </main>
     </div>
   );
